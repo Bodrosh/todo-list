0b7e2a5cffd742273da222008dcbce7326c9e716
Andrey Novikov <bodrovshum@gmail.com>
2023-12-20 11:55:34 +0300

    Fix Welcome icon bug, links style

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -37,15 +37,13 @@ defineProps({
                 <Link
                     :href="route('login')"
                     class="tw-font-semibold tw-text-gray-600 hover:tw-text-gray-900 dark:tw-text-gray-400 dark:hover:tw-text-white focus:outline focus:outline-2 focus:tw-rounded-sm focus:outline-red-500"
-                >Log in</Link
-                >
+                >Log in</Link>
 
                 <Link
                     v-if="canRegister"
                     :href="route('register')"
                     class="ms-4 tw-font-semibold tw-text-gray-600 hover:tw-text-gray-900 dark:tw-text-gray-400 dark:hover:tw-text-white focus:outline focus:outline-2 focus:tw-rounded-sm focus:outline-red-500"
-                >Register</Link
-                >
+                >Register</Link>
             </template>
         </div>
 
@@ -149,6 +147,9 @@ defineProps({
 <style>
 .bg-dots-darker {
     background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+}
+.stroke-red-500 {
+    stroke: #ef4444;
 }
 @media (prefers-color-scheme: dark) {
     .dark\:bg-dots-lighter {
--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+//Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
